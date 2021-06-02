b2593058a2b7e25afdb34fc70b3519bbf4922c62
yc173 <zuzana.tumova@deutsche-boerse.com>
2021-06-02 11:11:28 +0200

    PR

--- src/main/java/com/zuzka/myshowcode/repository/OrderRepository.java
+++ src/main/java/com/zuzka/myshowcode/repository/OrderRepository.java
@@ -1,8 +1,8 @@
 package com.zuzka.myshowcode.repository;
 
 import com.zuzka.myshowcode.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
-public interface OrderRepository extends CrudRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @Query("select o from Order o where o.createDateTime >= :createDateTime")
     List<Order> findAllWithCreationDateTimeAfter(@Param("createDateTime") LocalDateTime createDateTime);
--- src/main/java/com/zuzka/myshowcode/repository/ProductRepository.java
+++ src/main/java/com/zuzka/myshowcode/repository/ProductRepository.java
@@ -1,13 +1,13 @@
 package com.zuzka.myshowcode.repository;
 
 import com.zuzka.myshowcode.entity.Product;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface ProductRepository extends CrudRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Optional<Product> findByName(String name);
 
